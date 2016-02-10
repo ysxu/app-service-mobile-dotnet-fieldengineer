52e96b20311c861150f12ea4da015998f82e67a6
ysxu <yuaxu@microsoft.com>
2016-02-10 15:03:13 -0800

    add auth entry

--- server/FieldEngineer/App_Start/RouteConfig.cs
+++ server/FieldEngineer/App_Start/RouteConfig.cs
@@ -18,6 +18,8 @@ namespace FieldEngineer
                 url: "{controller}/{action}/{id}",
                 defaults: new { action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute("Auth", ".auth/login/aad", ".auth/login/aad");
         }
     }
 }
